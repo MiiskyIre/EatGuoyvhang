82c9f212700effee542351ef58dc57826133057d
MiiskyIre <98333793+MiiskyIre@users.noreply.github.com>
2022-01-25 00:00:09 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -343,10 +343,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '郭宇航表示看不起你';
+    if (score <= 99) return '就这？';
+    if (score <= 149) return '郭宇航给你磕头了';
+    if (score <= 199) return '爹';
     return '人？';
 }
 
